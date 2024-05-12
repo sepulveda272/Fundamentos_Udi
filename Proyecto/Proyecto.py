6c3fd37b308697d83ca1beb444e79e2f8a337cbd
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2024-05-12 12:46:16 -0500

    feat: :sparkles: inicio del proyecto

---
+++ Proyecto/Proyecto.py
@@ -0,0 +1,76 @@
+import json
+
+with open("c:/Users/ADMIN/OneDrive/Documentos/Univesidad/Fundamentos_Udi/Proyecto/inicio.json", "r") as file:
+    inicio = json.load(file)
+
+with open("c:/Users/ADMIN/OneDrive/Documentos/Univesidad/Fundamentos_Udi/Proyecto/pacientes.json", "r") as file:
+    pacientes = json.load(file)
+
+""" while True:
+    usuario = input("ingrese el usuario: ")
+    password = input("ingrese el password: ")
+
+    if usuario  == inicio["cuenta"][0]["usuario"] and password == inicio["cuenta"][0]["password"]:
+        print("INGRESO EXITOSO")
+        break
+    else:
+        print("Usuario o password incorrecto: ") """
+
+while True:
+    print("Buenos dias que decea hacer: \n 1.Paciente \n 0.salir")
+    try:
+        op = int(input("Ingrese su opción: "))
+        if op == 1:
+            while True:
+                print("Que decea realizar: \n 1.Registrar nuevo Paciente \n 2.Actualizar Info \n 3.Mostrar Pacientes \n 4.Volver al menu anterios")
+                try:
+                    menuPacientes = int(input("Ingrese su opción: "))
+                    if menuPacientes == 1:
+                        while True:
+                            cedula = int(input("ingrese la identificacion del paciente: "))
+                            cedula_existente = False
+                            for paciente in pacientes["pacientes"]:
+                                if paciente["cedula"] == cedula:
+                                    cedula_existente = True
+                                    break
+                            if cedula_existente == False:
+                                nombre = input("ingrese el nombre del paciente: ")
+                                genero = input("ingrese el genero del paciente: ")
+                                while True:
+                                    edad = int(input("ingrese el edad del paciente: "))
+                                    if edad >= 1 :   
+                                        nuevo_paciente = {
+                                            "cedula": cedula,
+                                            "nombre": nombre,
+                                            "genero": genero,
+                                            "edad": edad
+                                        }
+                                        pacientes["pacientes"].append(nuevo_paciente)
+                                        with open("c:/Users/ADMIN/OneDrive/Documentos/Univesidad/Fundamentos_Udi/Proyecto/pacientes.json", "w") as file:
+                                            json.dump(pacientes, file)
+                                        print("Gracias por su informacion")
+                                        break
+                                    else:
+                                        print("la edad no puedo ser negativa")
+                                break
+                            else:
+                               print("La cédula ingresada ya está registrada. Inténtelo de nuevo.")
+                    elif menuPacientes == 2:
+                        
+                        print("hola")
+                    elif menuPacientes == 3:
+                        
+                        print("hola")
+                    elif menuPacientes == 4:
+                        break
+                    else:
+                        print("Opción no válida. Por favor, ingrese un número del 1 al 4.")
+                except ValueError:
+                    print("Por favor, ingrese solo números.")
+        elif op == 0:
+            print("Hasta luego, vuelva pronto")
+            break
+        else:
+            print("Opción no válida. Por favor, ingrese 0 o 1.")
+    except ValueError:
+        print("Por favor, ingrese solo números.")
