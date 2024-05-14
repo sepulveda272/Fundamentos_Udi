d8f67fa2fb3aec136629b27da1790168f93d7f8c
sepulveda272 <sepulvedarojasjuandavid@gmail.com>
2024-05-14 01:53:20 -0500

    feat: :sparkles: continuacion

--- Proyecto/Proyecto.py
+++ Proyecto/Proyecto.py
@@ -19,56 +19,90 @@ sentinela = True
         print("Usuario o password incorrecto: ") """
 
 while True:
-    print("Buenos dias que decea hacer: \n 1.Paciente \n 0.salir")
+    print("Buenos dias que decea hacer: \n 1.Paciente  \n 0.salir")
     try:
         op = int(input("Ingrese su opción: "))
         if op == 1:
             while True:
-                print("Que decea realizar: \n 1.Registrar nuevo Paciente \n 2.Actualizar Info \n 3.Mostrar Pacientes \n 4.Volver al menu anterios")
+                print("Que decea realizar: \n 1.Registrar nuevo Paciente \n 2.Editar Info de un paciente \n 3.Registar nuevos valores de una paciente ya existentes \n 4.Volver al menu anterios")
                 try:
                     menuPacientes = int(input("Ingrese su opción: "))
                     if menuPacientes == 1:
-                        while True:
-                            cedula = int(input("ingrese la identificacion del paciente: "))
-                            cedula_existente = False
-                            for paciente in pacientes["pacientes"]:
-                                if paciente["cedula"] == cedula:
-                                    cedula_existente = True
-                                    break
-                            if cedula_existente == False:
-                                nombre = input("ingrese el nombre del paciente: ")
-                                genero = input("ingrese el genero del paciente: ")
-                                while sentinela:
-                                    edad = int(input("ingrese el edad del paciente: "))
-                                    if edad >= 1:
-                                        while sentinela:   
-                                            altura = int(input("Ingrese su altura (centimetros): "))
-                                            if altura >= 60:
-                                                while sentinela:
-                                                    peso = int(input("Ingrese su peso (kilogramos): "))
-                                                    if peso >= 1:
-                                                        nuevo_paciente = {
-                                                            "cedula": cedula,
-                                                            "nombre": nombre,
-                                                            "genero": genero,
-                                                            "edad": edad,
-                                                            "altura": altura,
-                                                            "peso": peso
-                                                        }
-                                                        pacientes["pacientes"].append(nuevo_paciente)
-                                                        with open("Proyecto/pacientes.json", "w") as file:
-                                                            json.dump(pacientes, file)
-                                                        print("Gracias por su informacion")
-                                                        sentinela = False
-                                                    else:
-                                                        print("ingrese un peso coerente")
-                                            else:
-                                                print("ingrese una altura coerente")
-                                    else:
-                                        print("la edad no puedo ser negativa")
-                                break
-                            else:
-                               print("La cédula ingresada ya está registrada. Inténtelo de nuevo.")
+                        while sentinela:
+                            try:
+                                cedula = int(input("ingrese la identificacion del paciente: "))
+                                cedula_existente = False
+                                for paciente in pacientes["pacientes"]:
+                                    if paciente["cedula"] == cedula:
+                                        cedula_existente = True
+                                        break
+                                if cedula_existente == False:
+                                    while sentinela:
+                                        nombre = input("ingrese el nombre del paciente: ")
+                                        if nombre.strip() and all(caracter.isalpha() or caracter.isspace() for caracter in nombre):
+                                            while sentinela:
+                                                genero = input("ingrese el genero del paciente (H) Hombre o (M) Mujer: ").upper()
+                                                if genero == "H" or genero == "M":
+                                                    while sentinela:
+                                                        try:
+                                                            edad = int(input("ingrese el edad del paciente: "))
+                                                            if edad >= 1:
+                                                                while sentinela:
+                                                                    try:   
+                                                                        altura = int(input("Ingrese su altura (centimetros): "))
+                                                                        if altura >= 60:
+                                                                            while sentinela:
+                                                                                try:
+                                                                                    peso = int(input("Ingrese su peso (kilogramos): "))
+                                                                                    if peso >= 1:
+                                                                                        if genero == "H":
+                                                                                            genero = "Hombre"
+                                                                                        elif genero == "M":
+                                                                                            genero = "Mujer"
+                                                                                        else:
+                                                                                            print("no se que poner")
+                                                                                        
+                                                                                        imc = peso / (altura**2)
+                                                                                        imc = imc * 10000
+                                                                                        imc = round(imc,1)
+
+                                                                                        visitas = 1
+                                                                                        nuevo_paciente = {
+                                                                                            "cedula": cedula,
+                                                                                            "nombre": nombre,
+                                                                                            "genero": genero,
+                                                                                            "edad": edad,
+                                                                                            "altura": altura,
+                                                                                            "peso": peso,
+                                                                                            "IMC": imc,
+                                                                                            "visita": visitas
+                                                                                        }
+                                                                                        pacientes["pacientes"].append(nuevo_paciente)
+                                                                                        with open("Proyecto/pacientes.json", "w") as file:
+                                                                                            json.dump(pacientes, file)
+                                                                                        print("Gracias por su informacion")
+                                                                                        sentinela = False
+                                                                                    else:
+                                                                                        print("ingrese un peso coerente")
+                                                                                except ValueError:
+                                                                                    print("Por favor, ingrese solo números.")
+                                                                        else:
+                                                                            print("ingrese una altura coerente")
+                                                                    except ValueError:
+                                                                        print("Por favor, ingrese lo que le pide.")
+                                                            else:
+                                                                print("la edad no puedo ser negativa")
+                                                        except ValueError:
+                                                            print("Por favor, ingrese lo que le pide.")
+                                                    break
+                                                else:
+                                                    print("Por favor, ingrese una de las opciones que se muestra. ")
+                                        else:
+                                            print("Por favor, no deje este campo vacio.")
+                                else:
+                                   print("La cédula ingresada ya está registrada. Inténtelo de nuevo.")
+                            except ValueError:
+                                print("Por favor, ingrese lo que le pide.")
                     elif menuPacientes == 2:
                         
                         print("hola")
